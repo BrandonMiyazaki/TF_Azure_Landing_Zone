9e09746ab707fb98f1fd18323b58f18d4d294f31
BrandonMiyazaki <brandon@miyazaki.cloud>
2024-06-12 17:34:52 -0700

    updated providers list

--- landingzones/caf-mgmt-groups/cafmgmtgroups.tf
+++ landingzones/caf-mgmt-groups/cafmgmtgroups.tf
@@ -1,24 +1,3 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used.
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.74.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-# You can use the azurerm_client_config data resource to dynamically
-# extract connection settings from the provider configuration.
-
-data "azurerm_client_config" "core" {}
-
 # Call the caf-enterprise-scale module directly from the Terraform Registry
 # pinning to the latest version
 
---
+++ landingzones/caf-mgmt-groups/providers.tf
@@ -0,0 +1,20 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used.
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# You can use the azurerm_client_config data resource to dynamically
+# extract connection settings from the provider configuration.
+
+data "azurerm_client_config" "core" {}
