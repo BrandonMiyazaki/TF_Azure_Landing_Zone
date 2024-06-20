8bfa648d95d50fd7f7e5b1d0f407ee04a02fc43b
BrandonMiyazaki <brandon@miyazaki.cloud>
2024-06-20 10:17:14 -0700

    updated rg tags

--- landingzones/connectivity/vwan-secure-hub/resourcegroups.tf
+++ landingzones/connectivity/vwan-secure-hub/resourcegroups.tf
@@ -1,6 +1,6 @@
 # Resource Groups Virtual-WAN - Region 1
 resource "azurerm_resource_group" "region1-rg1" {
-  name     = "rg-${var.region1}-Virtual-WAN-Demo"
+  name     = "rg-${var.region1}-Virtual-WAN-HUB"
   location = var.region1
   tags = {
     Environment = var.environment_tag
@@ -9,10 +9,10 @@ resource "azurerm_resource_group" "region1-rg1" {
 }
 # Resource Groups Virtual-WAN - Region 2
 resource "azurerm_resource_group" "region2-rg1" {
-  name     = "rg-${var.region2}-Virtual-WAN-Demo"
+  name     = "rg-${var.region2}-Virtual-WAN-HUB"
   location = var.region2
   tags = {
     Environment = var.environment_tag
-    Function    = "Virtual-WAN-DemoLab-ResourceGroups"
+    Function    = "Virtual-WAN-HUB-ResourceGroups"
   }
 }
